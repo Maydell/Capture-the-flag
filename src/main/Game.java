417175d13e0beacdf829b738ebe5ff74aad958ab
Maydell <mats.stichel@gmail.com>
2013-04-23 15:06:45 +0200

    Graphics

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,14 +1,26 @@
 package main;
 
+import menu.Menu;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Game extends StateBasedGame {
+	
+	BasicGameState menu;
 
 	public Game() {
 		super("Capture the flag");
+	}
+
+	@Override
+	public void initStatesList(GameContainer gc) throws SlickException {
+		menu = new Menu();
+		addState(menu);
+		enterState(menu.getID());
 	}
 	
 	/**
@@ -20,11 +32,4 @@ public class Game extends StateBasedGame {
 		gc.setDisplayMode(800, 600, false);
 		gc.start();
 	}
-
-	@Override
-	public void initStatesList(GameContainer arg0) throws SlickException {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,25 +1,36 @@
 package menu;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.gui.MouseOverArea;
+import org.newdawn.slick.opengl.CompositeImageData;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Menu extends BasicGameState{
 
+	MouseOverArea quitButton;
+	MouseOverArea startButton;
+	
 	@Override
-	public void init(GameContainer arg0, StateBasedGame arg1)
+	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
-		// TODO Auto-generated method stub
+		Image button = new Image("grass.jpg");
+		startButton = new MouseOverArea(gc, button, 100, 40, 50, 50);
+//		startButton.setNormalColor(new Color(255, 0, 0));
+		quitButton = new MouseOverArea(gc, button, 100, 40, 50, 150);
+//		quitButton.setNormalColor(new Color(0, 255, 0));
 		
 	}
 
 	@Override
-	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
-		// TODO Auto-generated method stub
-		
+		startButton.render(gc, g);
+		quitButton.render(gc, g);
 	}
 
 	@Override
